ec8174b1765f46d759a93aee562e810b055deadc
k.kosmachev <k.kosmachev@mail.ru>
2023-05-03 11:54:49 +0300

    Adding script to print out data of p-value and some graphics of it

--- scripts/make_time_sireis.py
+++ scripts/make_time_sireis.py
@@ -1,20 +1,24 @@
 # This is a sample Python script.
+import sys
+
 import pandas as pd
 import matplotlib.pyplot as plt
-import functions as local
-import seaborn as sns
-from functions.functions import print_histogram, print_pie
-from numpy import ma
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.stattools import adfuller
 
 if __name__ == '__main__':
     df1 = pd.read_csv('../data/new_data/retail.csv', index_col=0, dtype={
         'Invoice': str,
         'Quantity': float,
-        'name': str
+        'name': str,
+        'Country': str
     })
+    df_dates = pd.read_csv('../data/dates.csv')
+    df_dates['date'] = pd.to_datetime(df_dates.date, dayfirst=True)
+    df1['InvoiceDate'] = pd.to_datetime(df1.InvoiceDate, yearfirst=True)
 
-    df1['Sum'] = df1['Price'] * df1['Quantity']
-    df1[['InvoiceDate', 'Sum']].groupby(['InvoiceDate']).sum().to_csv('../data/new_data/time_sires.csv')
-
+    df1['Trade_over'] = df1['Price'] * df1['Quantity']
+    # df1 = df_dates.merge(df1, how='left', left_on='date', right_on='InvoiceDate')
+    df1[['InvoiceDate', 'Trade_over']].groupby(['InvoiceDate']).sum().to_csv('../data/new_data/time_sires.csv')
 
 
